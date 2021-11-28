17211dd7a863f3016f26cf849141ae4f3892bc62
HowIChrgeLazer <HowIChrgeLazer@users.noreply.github.com>
2021-11-27 19:27:24 -0800

    first commit

---
+++ farmhand.js
@@ -0,0 +1,104 @@
+$(document).ready(function() {
+    chrome.storage.sync.get(null, function(items) {
+        let fhOpt = items;
+    
+        let imgArrow=chrome.runtime.getURL("img/arrow-up.svg");
+        let menuFull = ''
+        const menuTop = 
+        '<ul>\
+        <li>\
+        <div class="item-content"><div class="item-inner-ext"><fh-title1>Farm</fh-title1><fh-title2>Hand</fh-title2></div></div>\
+        </li>\
+        ';
+
+        let menuFqlTop = '<details><summary>Farm Quick Links<span class="icon"><img src="'+imgArrow+'"width="22" height="22"></span><span class="spacer"></span></summary></div>'
+        const menuFqlBottom = '</details><hr class="fh-menu">'
+
+        const fqlFarm =
+        '<li>\
+        <a href="xfarm.php?id='+fhOpt.fqlFarmId+'" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">My Farm</div></div></div></a>\
+        </li>';
+        const fqlExplore =
+        '<li>\
+        <a href="explore.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Explore</div></div></div></a>\
+        </li>';
+        const fqlFishing =
+        '<li>\
+        <a href="fish.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Fishing</div></div></div></a>\
+        </li>';
+        const fqlWorkshop =
+        '<li>\
+        <a href="workshop.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Workshop</div></div></div></a>\
+        </li>';
+
+        const menuTqlTop = '<details><summary>Town Quick Links<span class="icon"><img src="'+imgArrow+'"width="22" height="22"></span><span class="spacer"></span></summary></div>'
+        const menuTqlBottom = '</details><hr class="fh-menu">'
+
+        const tqlBank =
+        '<li>\
+        <a href="bank.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Bank</div></div></div></a>\
+        </li>';
+        const tqlStore =
+        '<li>\
+        <a href="store.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Country Store</div></div></div></a>\
+        </li>';
+        const tqlMarket =
+        '<li>\
+        <a href="market.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Farmer\'s Market</div></div></div></a>\    </li>';
+        const tqlPetShop =
+        '<li>\
+        <a href="pets.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Pet Shop</div></div></div></a>\
+        </li>';
+        const tqlPetItemCollect =
+        '<li>\
+        <a href="allpetitems.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Pet Item Collection</div></div></div></a>\
+        </li>';
+        const tqlPostOffice =
+        '<li>\
+        <a href="postoffice.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Post Office</div></div></div></a>\
+        </li>';
+        const tqlSteakMarket =
+        '<li>\
+        <a href="steakmarket.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Steak Market</div></div></div></a>\
+        </li>';
+        const tqlWell =
+        '<li>\
+        <a href="well.php" data-view=".view-main" class="item-link close-panel"><div class="item-content"><div class="item-inner"><div class="item-title">Wishing Well</div></div></div></a>\
+        </li>';
+
+        const menuBottom =
+        '<div class="item-content"><div class="item-inner-ext"><button id="go-to-options">FarmHand Options</button></div></div>\
+        </ul>\
+        ';
+
+        // Build FarmHand Menu
+        menuFull += menuTop;
+        menuFull += menuFqlTop;
+        if(fhOpt.fqlFarm) { menuFull += fqlFarm; }
+        if(fhOpt.fqlExplore) { menuFull += fqlExplore; }
+        if(fhOpt.fqlFishing) { menuFull += fqlFishing; }
+        if(fhOpt.fqlWorkshop) { menuFull += fqlWorkshop; }
+        menuFull += menuFqlBottom;
+        menuFull += menuTqlTop;
+        if(fhOpt.tqlBank) { menuFull += tqlBank; }
+        if(fhOpt.tqlStore) { menuFull += tqlStore; }
+        if(fhOpt.tqlMarket) { menuFull += tqlMarket; }
+        if(fhOpt.tqlPetShop) { menuFull += tqlPetShop; }
+        if(fhOpt.tqlPetItemCollect) { menuFull += tqlPetItemCollect; }
+        if(fhOpt.tqlPostOffice) { menuFull += tqlPostOffice; }
+        if(fhOpt.tqlSteakMarket) { menuFull += tqlSteakMarket; }
+        if(fhOpt.tqlWell) { menuFull += tqlWell; }
+        menuFull += menuTqlBottom;
+        menuFull += menuBottom;
+
+        $(".list-block:eq(2)").append(menuFull);
+        
+        document.querySelector('#go-to-options').addEventListener('click', function() {
+            if (chrome.runtime.openOptionsPage) {
+              chrome.runtime.openOptionsPage();
+            } else {
+              window.open(chrome.runtime.getURL('options.html'));
+            }
+        });
+    });
+});
---
+++ options.js
@@ -0,0 +1,78 @@
+// Saves options to chrome.storage
+function save_options() {
+    var fqlFarmId = document.getElementById('fqlFarmId').value;
+    var fqlFarm = document.getElementById('fqlFarm').checked;
+    var fqlExplore = document.getElementById('fqlExplore').checked;
+    var fqlFishing = document.getElementById('fqlFishing').checked;
+    var fqlWorkshop = document.getElementById('fqlWorkshop').checked;
+
+    var tqlStore = document.getElementById('tqlStore').checked;
+    var tqlMarket = document.getElementById('tqlMarket').checked;
+    var tqlPetShop = document.getElementById('tqlPetShop').checked;
+    var tqlPetItemCollect = document.getElementById('tqlPetItemCollect').checked;
+    var tqlPostOffice = document.getElementById('tqlPostOffice').checked;
+    var tqlSteakMarket = document.getElementById('tqlSteakMarket').checked;
+    var tqlWell = document.getElementById('tqlWell').checked;
+    chrome.storage.sync.set({
+        // Farm quick links settings
+        fqlFarmId: fqlFarmId,
+        fqlFarm: fqlFarm,
+        fqlExplore: fqlExplore,
+        fqlFishing: fqlFishing,
+        fqlWorkshop: fqlWorkshop,
+        // Town quick links settings
+        tqlBank: tqlBank,
+        tqlStore: tqlStore,
+        tqlMarket: tqlMarket,
+        tqlPetShop: tqlPetShop,
+        tqlPetItemCollect: tqlPetItemCollect,
+        tqlPostOffice: tqlPostOffice,
+        tqlSteakMarket: tqlSteakMarket,
+        tqlWell: tqlWell
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+        status.textContent = '';
+        }, 750);
+    });
+  }
+  
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Default values
+    chrome.storage.sync.get({
+        fqlFarmId: '0',
+        fqlFarm: false,
+        fqlExplore: true,
+        fqlFishing: true,
+        fqlWorkshop: true,
+        tqlBank: true,
+        tqlStore: true,
+        tqlMarket: true,
+        tqlPetShop: true,
+        tqlPetItemCollect: false,
+        tqlPostOffice: true,
+        tqlSteakMarket: true,
+        tqlWell: true
+    }, function(items) {
+        document.getElementById('fqlFarmId').value = items.fqlFarmId;
+        document.getElementById('fqlFarm').checked = items.fqlFarm;
+        document.getElementById('fqlExplore').checked = items.fqlExplore;
+        document.getElementById('fqlFishing').checked = items.fqlFishing;
+        document.getElementById('fqlWorkshop').checked = items.fqlWorkshop;
+        document.getElementById('tqlBank').checked = items.tqlBank;
+        document.getElementById('tqlStore').checked = items.tqlStore;
+        document.getElementById('tqlMarket').checked = items.tqlMarket;
+        document.getElementById('tqlPetShop').checked = items.tqlPetShop;
+        document.getElementById('tqlPetItemCollect').checked = items.tqlPetItemCollect;
+        document.getElementById('tqlPostOffice').checked = items.tqlPostOffice;
+        document.getElementById('tqlSteakMarket').checked = items.tqlSteakMarket;
+        document.getElementById('tqlWell').checked = items.tqlWell;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
