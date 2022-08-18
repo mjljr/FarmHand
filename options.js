f69b3c173b2e923c9bdeba40ad8a82bce8b64182
camwick <cwickers@pnw.edu>
2022-08-17 21:12:56 -0500

    reordered stuff to match order of FarmRPG

--- options.js
+++ options.js
@@ -6,26 +6,26 @@ function save_options() {
 
     var fqlFarmId = document.getElementById('fqlFarmId').value;
     var fqlFarm = document.getElementById('fqlFarm').checked;
-    var fqlRaptor = document.getElementById('fqlRaptor').checked;
-    var fqlExplore = document.getElementById('fqlExplore').checked;
-    var fqlFishing = document.getElementById('fqlFishing').checked;
-    var fqlQuests = document.getElementById('fqlQuests').checked;
     var fqlWorkshop = document.getElementById('fqlWorkshop').checked;
+    var fqlFishing = document.getElementById('fqlFishing').checked;
+    var fqlExplore = document.getElementById('fqlExplore').checked;
+    var fqlQuests = document.getElementById('fqlQuests').checked;
     var fqlCraftworks = document.getElementById('fqlCraftworks').checked;
+    var fqlRaptor = document.getElementById('fqlRaptor').checked;
 
-    var tqlBank = document.getElementById('tqlBank').checked;
     var tqlStore = document.getElementById('tqlStore').checked;
     var tqlMarket = document.getElementById('tqlMarket').checked;
+    var tqlBorgen = document.getElementById('tqlBorgen').checked;
+    var tqlBank = document.getElementById('tqlBank').checked;
+    var tqlVault = document.getElementById('tqlVault').checked;
+    var tqlSteakMarket = document.getElementById('tqlSteakMarket').checked;
+    var tqlPostOffice = document.getElementById('tqlPostOffice').checked;
     var tqlPetShop = document.getElementById('tqlPetShop').checked;
     var tqlPetItemCollect = document.getElementById('tqlPetItemCollect').checked;
-    var tqlPostOffice = document.getElementById('tqlPostOffice').checked;
-    var tqlSteakMarket = document.getElementById('tqlSteakMarket').checked;
     var tqlWell = document.getElementById('tqlWell').checked;
-    var tqlCommunity = document.getElementById('tqlCommunity').checked;
-    var tqlLocksmith = document.getElementById('tqlLocksmith').checked;
-    var tqlVault = document.getElementById('tqlVault').checked;
     var tqlExchange = document.getElementById('tqlExchange').checked;
-    var tqlBorgen = document.getElementById('tqlBorgen').checked;
+    var tqlLocksmith = document.getElementById('tqlLocksmith').checked;
+    var tqlCommunity = document.getElementById('tqlCommunity').checked;
     chrome.storage.sync.set({
         // Chat settings
         chatUsername: chatUsername,
@@ -34,26 +34,26 @@ function save_options() {
         // Farm quick links settings
         fqlFarmId: fqlFarmId,
         fqlFarm: fqlFarm,
-        fqlRaptor: fqlRaptor,
-        fqlExplore: fqlExplore,
-        fqlFishing: fqlFishing,
-        fqlQuests: fqlQuests,
         fqlWorkshop: fqlWorkshop,
+        fqlFishing: fqlFishing,
+        fqlExplore: fqlExplore,
+        fqlQuests: fqlQuests,
         fqlCraftworks: fqlCraftworks,
+        fqlRaptor: fqlRaptor,
         // Town quick links settings
-        tqlBank: tqlBank,
         tqlStore: tqlStore,
         tqlMarket: tqlMarket,
+        tqlBorgen: tqlBorgen,
+        tqlBank: tqlBank,
+        tqlVault: tqlVault,
+        tqlSteakMarket: tqlSteakMarket,
+        tqlPostOffice: tqlPostOffice,
         tqlPetShop: tqlPetShop,
         tqlPetItemCollect: tqlPetItemCollect,
-        tqlPostOffice: tqlPostOffice,
-        tqlSteakMarket: tqlSteakMarket,
         tqlWell: tqlWell,
-        tqlCommunity: tqlCommunity,
-        tqlLocksmith: tqlLocksmith,
-        tqlVault: tqlVault,
         tqlExchange: tqlExchange,
-        tqlBorgen: tqlBorgen
+        tqlLocksmith: tqlLocksmith,
+        tqlCommunity: tqlCommunity
     }, function() {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -74,50 +74,53 @@ function restore_options() {
         chatNotif: false,
         fqlFarmId: '0',
         fqlFarm: false,
-        fqlRaptor: false,
-        fqlExplore: true,
-        fqlFishing: true,
-        fqlQuests: false,
         fqlWorkshop: true,
+        fqlFishing: true,
+        fqlExplore: true,
+        fqlQuests: false,
         fqlCraftworks: false,
-        tqlBank: true,
+        fqlRaptor: false,
         tqlStore: true,
         tqlMarket: true,
+        tqlBorgen: false,
+        tqlBank: true,
+        tqlVault: false,
+        tqlSteakMarket: true,
+        tqlPostOffice: true,
         tqlPetShop: true,
         tqlPetItemCollect: false,
-        tqlPostOffice: true,
-        tqlSteakMarket: true,
         tqlWell: true,
-        tqlCommunity: false,
-        tqlLocksmith: false,
-        tqlVault: false,
         tqlExchange: false,
-        tqlBorgen: false
+        tqlLocksmith: false,
+        tqlCommunity: false
+        
     }, function(items) {
         document.getElementById('chatUsername').value = items.chatUsername;
         document.getElementById('chatColor').value = items.chatColor;
         document.getElementById('chatNotif').checked = items.chatNotif;
         document.getElementById('fqlFarmId').value = items.fqlFarmId;
         document.getElementById('fqlFarm').checked = items.fqlFarm;
-        document.getElementById('fqlRaptor').checked = items.fqlRaptor;
-        document.getElementById('fqlExplore').checked = items.fqlExplore;
-        document.getElementById('fqlFishing').checked = items.fqlFishing;
-        document.getElementById('fqlQuests').checked = items.fqlQuests;
         document.getElementById('fqlWorkshop').checked = items.fqlWorkshop;
+        document.getElementById('fqlFishing').checked = items.fqlFishing;
+        document.getElementById('fqlExplore').checked = items.fqlExplore;
+        document.getElementById('fqlQuests').checked = items.fqlQuests;
         document.getElementById('fqlCraftworks').checked = items.fqlCraftworks;
-        document.getElementById('tqlBank').checked = items.tqlBank;
+        document.getElementById('fqlRaptor').checked = items.fqlRaptor;
+
+        
         document.getElementById('tqlStore').checked = items.tqlStore;
         document.getElementById('tqlMarket').checked = items.tqlMarket;
+        document.getElementById('tqlBorgen').checked = items.tqlBorgen;
+        document.getElementById('tqlBank').checked = items.tqlBank;
+        document.getElementById('tqlVault').checked = items.tqlVault;
+        document.getElementById('tqlSteakMarket').checked = items.tqlSteakMarket;
+        document.getElementById('tqlPostOffice').checked = items.tqlPostOffice;
         document.getElementById('tqlPetShop').checked = items.tqlPetShop;
         document.getElementById('tqlPetItemCollect').checked = items.tqlPetItemCollect;
-        document.getElementById('tqlPostOffice').checked = items.tqlPostOffice;
-        document.getElementById('tqlSteakMarket').checked = items.tqlSteakMarket;
-        document.getElementById('tqlWell').checked = items.tqlWell;
-        document.getElementById('tqlCommunity').checked = items.tqlCommunity;
-        document.getElementById('tqlLocksmith').checked = items.tqlLocksmith;
-        document.getElementById('tqlVault').checked = items.tqlVault;
         document.getElementById('tqlExchange').checked = items.tqlExchange;
-        document.getElementById('tqlBorgen').checked = items.tqlBorgen;
+        document.getElementById('tqlWell').checked = items.tqlWell;
+        document.getElementById('tqlLocksmith').checked = items.tqlLocksmith;
+        document.getElementById('tqlCommunity').checked = items.tqlCommunity;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
